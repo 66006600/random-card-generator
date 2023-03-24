909dbdc677b157d48b42feceb8230cef5801d919
Beverly M <bevponi@gmail.com>
2023-03-24 20:25:50 +0000

    Viernes pm

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-function carta () {
-    
+window.onload = function carta() {
+
+   
     let pintas = ["Diamantes", "Corazones", "Picas", "Clubes"];
     let valores = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Rey", "Reina", "As"];
     
-    let palo = pintas[Math.floor(Math.random() * pintas.length)];
-    let valor = valores[Math.floor(Math.random() * valores.length)];
-    
+   
+      
+    var resultado1 = document.getElementById("h2");
+    resultado1.innerHtml = pintas[getRandomInt(pintas.length)];
 
-    document.getElementsByTagName("h2").innerHtml = palo;
-    document.getElementsByTagName("h1").innerHtml = valor;
+    var resultado2 = document.getElementById("h1");
+    resultado2.innerHtml = valores[getRandomInt(valores.length)];
+
+    
 
 
 
