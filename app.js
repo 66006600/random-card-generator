062cc775740dfb10fa48b0750d5fa11115cecf8c
Beverly M <bevponi@gmail.com>
2023-03-27 15:02:32 +0000

    Almost

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-window.onload = setInterval(carta,10000);
+window.onload = setInterval(carta,2000);
 
 function carta() {
-  let suits = ["♦", " ♥ ", "♠", "♣"];
 
+  let suits = ["♦", " ♥ ", "♠", "♣"];
+ 
   let valores = ["2","3","4","5","6","7","8","9","10","Rey","Reina",
     "As",
   ];  
@@ -12,9 +13,7 @@ function carta() {
   h2[0].innerText = suit;
   h2[1].innerText = suit;
   console.log(h2.innerText);
-
-  let redHeart = document.getElementsByTagName("h2");
-
+   
   const p = document.getElementsByTagName("p");
   p[0].innerText = valores[getRandom(valores.length)];
   console.log(p[0].innerText);
