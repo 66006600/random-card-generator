70985f3699f4653188df5e2f26121f30d5591f12
Beverly M <bevponi@gmail.com>
2023-03-28 19:55:13 +0000

    solofalta color

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-window.onload = setInterval(carta,2000);
+window.onload = setInterval(carta, 2000);
 
 function carta() {
+  let suits = ["♦", "♥", "♠", "♣"];
 
-  let suits = ["♦", " ♥ ", "♠", "♣"];
- 
-  let valores = ["2","3","4","5","6","7","8","9","10","Rey","Reina",
+  let valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Rey",
+    "Reina",
     "As",
-  ];  
+  ];
 
   const h2 = document.getElementsByTagName("h2");
   const suit = suits[getRandom(suits.length)];
+
   h2[0].innerText = suit;
   h2[1].innerText = suit;
   console.log(h2.innerText);
-   
+
   const p = document.getElementsByTagName("p");
   p[0].innerText = valores[getRandom(valores.length)];
   console.log(p[0].innerText);
@@ -22,5 +33,3 @@ function carta() {
     return Math.floor(Math.random() * max);
   }
 }
-
-
