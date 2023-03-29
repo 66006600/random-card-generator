ec6a195638a2e0eab14aeecf6cf12cf952c3506a
Beverly M <bevponi@gmail.com>
2023-03-29 00:04:05 +0000

    Finished

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 window.onload = setInterval(carta, 2000);
 
 function carta() {
-  let suits = ["♦", "♥", "♠", "♣"];
+  let suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
+  let suitLogos = ["♦", "♥", "♠", "♣"];
 
   let valores = [
     "2",
@@ -19,10 +20,15 @@ function carta() {
   ];
 
   const h2 = document.getElementsByTagName("h2");
-  const suit = suits[getRandom(suits.length)];
+  const suitIndex = getRandom(suits.length);
+  const suit = suits[suitIndex];
+  const suitLogo = suitLogos[suitIndex];
+  const color = getColor(suit);
 
-  h2[0].innerText = suit;
-  h2[1].innerText = suit;
+  h2[0].innerText = suitLogo;
+  h2[0].style.color = color;
+  h2[1].innerText = suitLogo;
+  h2[1].style.color = color;
   console.log(h2.innerText);
 
   const p = document.getElementsByTagName("p");
@@ -31,5 +37,16 @@ function carta() {
 
   function getRandom(max) {
     return Math.floor(Math.random() * max);
+  }
+
+  function getColor(suit) {
+    console.log(suit);
+    if (suit === "Hearts" || suit === "Diamonds") {
+      console.log("red");
+      return "red";
+    } else {
+      console.log("black");
+      return "black";
+    }
   }
 }
